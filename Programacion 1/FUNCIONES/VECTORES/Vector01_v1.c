5061d152e0bcd025c298a8c68b58de8323fe6e07
BehringerAJB <behringerAJB@users.noreply.github.com>
2023-07-25 13:21:13 -0300

    asi estaba originalmente con una carga por un ciclo exacto

---
+++ Programacion 1/FUNCIONES/VECTORES/Vector01_v1.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#define TAMANO_VECTOR 5
+// este programa , sirve para cargar un vector
+int main() {
+    int vector[TAMANO_VECTOR];{0}
+    int i;
+    printf("\n Vecor Vacio\n");
+ 
+   
+    for (i = 0; i < TAMANO_VECTOR; i++) 
+    {
+        printf("vector[%d]=%2d\n ",i,vector[i]);
+    }
+  
+    // Inicializar el vector con valores
+    for (i = 0; i < TAMANO_VECTOR; i++) {
+        vector[i] = i + 1;
+    } 
+
+    // Imprimir el vector
+    printf("\nValores del Vector:\n\n ");
+    for (i = 0; i < TAMANO_VECTOR; i++) 
+    {
+        printf("vector[%d]=%2d\n ",i,vector[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
